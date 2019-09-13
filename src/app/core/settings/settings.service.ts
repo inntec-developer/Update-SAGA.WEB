d84b0f32d3ae988b19fbf4005d5ec730bc4807ab
Nestor Iniguez <nextor.ivan@icloud.com>
2019-09-13 10:09:46 -0500

    Actualizar Version V2.0.109131004

--- src/app/core/settings/settings.service.ts
+++ src/app/core/settings/settings.service.ts
@@ -39,8 +39,8 @@ export class SettingsService {
     // App Settings
     // -----------------------------------
     this.app = {
-      vertionTime: '20190912_1030_DVP-SBI610/20190912_1030_DVP-SBI611',
-      vertion: 'v2.0.109121120',
+      vertionTime: '20190913_1004_DVP-SBI610/20190913_1004_DVP-SBI611',
+      vertion: 'v2.0.109131004',
       environment: 'BETA',
       name: 'SAGA - Inntec',
       description: 'Sistema Administrativo de Gestión Avanzada',
